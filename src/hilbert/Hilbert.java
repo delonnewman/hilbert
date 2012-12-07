f0cd7d3e39a5016f0ce4505817f00f59fdda319e
Delon Newman <delon.newman@gmail.com>
2012-12-07 13:14:35 -1000

    Hilbert can read rapla document from command-line

--- src/hilbert/Hilbert.java
+++ src/hilbert/Hilbert.java
@@ -4,16 +4,37 @@
  */
 package hilbert;
 
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.dom4j.dom.DOMDocument;
+
 /**
  *
  * @author delon
  */
 public class Hilbert {
+    private static boolean DEBUG = true;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        if ( args.length < 1 ) {
+            System.out.println("Usage: hilbert RAPLA_EXPORT");
+        }
+        else {
+            String path = args[0];
+
+            try {
+                String xml = FileUtils.readFileToString(new File(path));
+                DOMDocument doc = new DOMDocument(xml);
+                System.out.println(doc);
+            }
+            catch (IOException e) {
+                System.out.println("Can't read from '" + path + "'");
+                if ( DEBUG ) { e.printStackTrace(); }
+            }
+        }
     }
 }
